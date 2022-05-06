4cbda22d339e5a230e802c627a75b682601c7e2a
gitlantis <asliddin.oripov@gmail.com>
2022-05-06 23:43:06 +0300

    plugin functionality

--- singpass-wordpress-plugin.php
+++ singpass-wordpress-plugin.php
@@ -14,55 +14,84 @@ require_once dirname(__FILE__) . '/vendor/autoload.php';
 
 use GuzzleHttp\Client;
 use Firebase\JWT\JWT;
-// use Firebase\JWT\Key;
+use Firebase\JWT\JWK;
+use Firebase\JWT\Key;
 
 function singpass_button()
 {
 	$path = plugin_dir_url(__FILE__);
-	echo '<br>';
-	echo 'Login with <img src=' . $path . 'assets/singpass_logo_fullcolours.png width=100px>';
+	echo '<div class="d-flex justify-content-center" data-bs-toggle="modal" data-bs-target="#qr_code_modal">
+	<a class="btn btn-outline-secondary btn-lg p-1"><img src=' . $path . 'assets/singpass_logo_fullcolours.png width=100px /></a>
+	</div>';
+
 	//	echo '<button type="button" class="btn btn-outline-secondary">Secondary</button>';
-	echo '<br>';
 }
 
 function singpass_jwks()
 {
-	$file = file_get_contents('jwks_keys', true);
-	echo $file;
+	//$file = file_get_contents('jwks_keys', true);
+	echo get_option('public_jwks');
 }
 
 function oidc_signin_callback($params)
 {
+	$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ? 'https' : 'http';
+    $url = $protocol . "://$_SERVER[HTTP_HOST]";
+    $plugin_name = explode('/', plugin_basename(__FILE__))[0];
+
+	$sig_kid = '';
+	$encPrivateKey = null;	
+
 	//try {
 	$code = $params->get_param('code');
 	$state = $params->get_param('state');
+	$token_url = get_option('token_url');//'https://stg-id.singpass.gov.sg/token';
+	$callback_url = get_option('callback_url');//"$url/wp-json/singpass/v1/signin_oidc";
+	$parser_url = get_option('token_parser_url');//'http://asliddin-jwt.socialservicesconnect.com:5000/parser';
+	//$parser_url = 'http://ec2-52-59-238-40.eu-central-1.compute.amazonaws.com:5000/parser';
 
-	$sigPrivateKey = <<<EOD
-		-----BEGIN PRIVATE KEY-----
-		MEECAQAwEwYHKoZIzj0CAQYIKoZIzj0DAQcEJzAlAgEBBCAn2IkQq8dNpSxE+u5l
-		Awme+XPDnCkWp9+NvhrcW+tS7A==
-		-----END PRIVATE KEY-----
-		EOD;
+	$singpass_client = get_option('singpass_client');//'hCqn1a2gQFi6QLPeaw3LIWP3LQ2E5f0r';
+	$sigPrivateKey = get_option('private_sig_key');
 
+	$public_jwks = json_decode(get_option('public_jwks'));
+	foreach($public_jwks->{'keys'} as $jwk){
+		if(strcmp($jwk->{'use'},'sig')==0) $sig_kid = $jwk->{'kid'};
+	}
+
+	$private_jwks = json_decode(get_option('private_jwks'));
+	foreach($private_jwks->{'keys'} as $jwk){
+		if(strcmp($jwk->{'use'},'enc')==0) $encPrivateKey = $jwk;
+	}
+
+	// $sig_kid = 'octopus8_sig_key_01';
+	// $encPrivateKey = json_decode('{"kty": "EC","d": "cV6QfdH46rZ1t5qYAq9IiZOmkxbQxoU1S_oYr0BDYdI","use": "enc","crv": "P-256","kid": "octopus8_enc_key_01","x": "OZ0iGy9uaK-esgDx021JalqAh8Kyop4m0v8OvSSq5UQ","y": "httcDJHMKWVQ3vtiBKXJRnUcPpYdojzXT2IhdFVpFLw","alg": "ECDH-ES+A128KW"}');
+	// $sigPrivateKey = <<<EOD
+	// 	-----BEGIN PRIVATE KEY-----
+	// 	MEECAQAwEwYHKoZIzj0CAQYIKoZIzj0DAQcEJzAlAgEBBCAn2IkQq8dNpSxE+u5l
+	// 	Awme+XPDnCkWp9+NvhrcW+tS7A==
+	// 	-----END PRIVATE KEY-----
+	// 	EOD;
+
+	$domain = parse_url($token_url);
 	$payload = array(
-		"iss" => "hCqn1a2gQFi6QLPeaw3LIWP3LQ2E5f0r",
-		"sub" => "hCqn1a2gQFi6QLPeaw3LIWP3LQ2E5f0r",
-		"aud" => "https://stg-id.singpass.gov.sg",
+		"iss" => $singpass_client,
+		"sub" => $singpass_client,
+		"aud" => $domain['scheme'] . '://' . $domain['host'],
 		"exp" => strtotime($Date . '+2 mins'),
 		"iat" => strtotime($Date . '+0 mins')
 	);
 
-	$token = JWT::encode($payload, $sigPrivateKey, 'ES256', 'octopus8_sig_key_01');
-	$token_url = 'https://stg-id.singpass.gov.sg/token';
-
+	// $token = JWT::encode($payload, $sigPrivateKey, 'ES256', $sig_kid);
+	$token = JWT::encode($payload, $sigPrivateKey, 'ES256', $sig_kid);
+	var_dump($token);
 	$body = array(
 		'code' => $code,
 		'client_assertion_type' => 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
 		'client_assertion' => $token,
-		'client_id' => 'hCqn1a2gQFi6QLPeaw3LIWP3LQ2E5f0r',
+		'client_id' => $singpass_client,
 		'scope' => 'openid',
 		'grant_type' => 'authorization_code',
-		'redirect_uri' => 'https://asliddin.socialservicesconnect.com/wp-json/singpass/v1/signin_oidc'
+		'redirect_uri' => $callback_url
 	);
 
 	$headers = array(
@@ -89,7 +118,6 @@ function oidc_signin_callback($params)
 
 	$jwt = json_decode($response);
 	var_dump($response);
-	$encPrivateKey = json_decode('{"kty": "EC","d": "cV6QfdH46rZ1t5qYAq9IiZOmkxbQxoU1S_oYr0BDYdI","use": "enc","crv": "P-256","kid": "octopus8_enc_key_01","x": "OZ0iGy9uaK-esgDx021JalqAh8Kyop4m0v8OvSSq5UQ","y": "httcDJHMKWVQ3vtiBKXJRnUcPpYdojzXT2IhdFVpFLw","alg": "ECDH-ES+A128KW"}');
 
 	$body = json_encode(array(
 		'key' => $encPrivateKey,
@@ -102,11 +130,8 @@ function oidc_signin_callback($params)
 		'Content-Type' => 'application/json',
 	];
 
-	//$url = 'http://ec2-52-59-238-40.eu-central-1.compute.amazonaws.com:5000/parser';
-	$url = 'http://asliddin-jwt.socialservicesconnect.com:5000/parser';
-	
 	$client = new Client();
-	$res = $client->request('POST', $url, [
+	$res = $client->request('POST', $parser_url, [
 		'headers' => $headers,
 		'body' => $body
 	]);
@@ -145,31 +170,39 @@ function header_scripts()
 	qr_code_scripts();
 }
 
-// function add_admin_page()
-// {
-// 	// add_admin_page('Singpass', 'Singpass plugin', 'manage_options', 'singpass_plugin', array('admin_page'), plugin_url('/assets/og_image_mini.svg', __FILE__), null);
-// 	add_menu_page('Singpass', 'Singpass plugin', 'manage_options', '');
-// }
-
-// function admin_page(){
-
-// }
-function add_admin_page() {
-    add_menu_page( 'Singpass plugin', 'Singpass', 'manage_options', 'singpass-page.php', 'singpass_admin_page', plugins_url('/assets/og_image_mini.png',  __FILE__) );
+function add_admin_page()
+{
+	add_menu_page('Singpass plugin', 'Singpass', 'manage_options', 'singpass-page.php', 'singpass_admin_page', plugins_url('/assets/og_image_mini.png',  __FILE__));
 }
 
-function singpass_admin_page(){
-    require_once plugin_dir_path(__FILE__).'/singpass-page.php';
+function singpass_admin_page()
+{
+	require_once plugin_dir_path(__FILE__) . '/singpass-page.php';
 }
 
-function settings_link($links){
+function settings_link($links)
+{
 	$settins_link = '<a href="admin.php?page=singpass-page.php">Settings</a>';
 	array_push($links, $settins_link);
 	return $links;
 }
 
-$plugin_name = plugin_basename(__FILE__);
+function create_settings()
+{
+	$plugin_name = explode('/', plugin_basename(__FILE__))[0];
+	register_setting("$plugin_name._settings", "token_url");
+	register_setting("$plugin_name._settings", "callback_url");
+	register_setting("$plugin_name._settings", "token_parser_url");
+	register_setting("$plugin_name._settings", "singpass_client");
+	register_setting("$plugin_name._settings", "jwk_endpoint");
+	register_setting("$plugin_name._settings", "public_jwks");
+	register_setting("$plugin_name._settings", "private_jwks");
+	register_setting("$plugin_name._settings", "private_sig_key");
+	register_setting("$plugin_name._settings", "private_enc_key");
+}
 
+$plugin_name = plugin_basename(__FILE__);
+add_action('admin_init', 'create_settings');
 add_filter("plugin_action_links_$plugin_name", 'settings_link');
 add_action('admin_menu', 'add_admin_page');
 add_action('login_head', 'header_scripts');
@@ -189,6 +222,10 @@ add_action('rest_api_init', function () {
 	));
 });
 
+wp_register_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js');
+wp_enqueue_script('bootstrap-js');
+wp_register_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
+wp_enqueue_style('bootstrap-css');
 ///(?P<id>[\d]+)
 //http://localhost/singpass/wp-json/singpass/v1/jwks
 //http://asliddin.socialservicesconnect.com/wp-json/singpass/v1/jwks
